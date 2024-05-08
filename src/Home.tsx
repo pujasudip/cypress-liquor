52eb580f266048078ae5a03c8e441a69be9fc942
Sudip Baral <sudip.baral@Sudips-MacBook-Pro.local>
2024-05-07 21:33:15 -0700

    api hook up

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const Home: React.FC = () => {
+  const getData = async () => {
+    const res = await axios({
+      method: "GET",
+      url: "http://www.cypress-liquor-be.shop/get-users",
+    });
+    console.log("res:", res);
+  };
+  useEffect(() => {
+    getData();
+  });
   return <div>This is a test website.</div>;
 };
 
