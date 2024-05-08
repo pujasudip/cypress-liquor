1eaaea65a097cdf3bf7bd87205a363cda3cf58a4
Sudip Baral <sudip.baral@Sudips-MacBook-Pro.local>
2024-05-07 18:41:42 -0700

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './App.css';
-import Home from './Home';
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about-us",
+    element: <div>Hello ke cha?</div>,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
