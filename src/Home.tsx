13ef0f1ac98d6d850be563ed5ac5a4603fe0b6d6
Sudip Baral <sudip.baral@Sudips-MacBook-Pro.local>
2024-05-07 18:24:23 -0700

    wip

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,23 +1,7 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
 const Home: React.FC = () => {
-  const [data, setData] = useState<any>();
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:5000", {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-          "Content-Type": "json",
-        },
-      })
-      .then((res: any) => {
-        console.log("res:", typeof res.data);
-        setData(res.data.json());
-      });
-  });
-  return <div>{data}</div>;
+  return <div>This is a test website.</div>;
 };
 
 export default Home;
