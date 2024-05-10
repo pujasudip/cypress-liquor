9fa2107884cc326053dacd348a7e82f598aa0b2a
Sudip Baral <sudip.baral@Sudips-MacBook-Pro.local>
2024-05-10 02:05:54 -0700

    cors issue fixed

--- src/logins/Login.tsx
+++ src/logins/Login.tsx
@@ -48,7 +48,7 @@ const Login = () => {
   const isMobile = useMobile(window.innerWidth);
 
   const handleLogin = async (values: any) => {
-    const res = await fetch("http://127.0.0.1:5000/login", {
+    const res = await fetch("http://cypress-liquor-be.shop/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
